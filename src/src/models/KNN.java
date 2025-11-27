34a0d9b397795e508f49ed32a2336edb6faa84fc
DavidAriesan <davidariesan2@gmail.com>
2025-11-27 18:44:11 +0200

    Created KNN Model Class

---
+++ src/src/models/KNN.java
@@ -0,0 +1,20 @@
+package models;
+
+
+import core.Instance;
+import core.Model;
+
+import java.util.List;
+
+public class KNN<F, L> implements Model<F, L> {
+
+    @Override
+    public void train(List<Instance<F, L>> instances) {
+
+    }
+
+    @Override
+    public void test(List<Instance<F, L>> instances) {
+
+    }
+}
