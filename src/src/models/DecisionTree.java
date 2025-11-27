79d98ef41f8633e0e2a70110c680535f9b82520b
DavidAriesan <davidariesan2@gmail.com>
2025-11-27 18:45:00 +0200

    Created DecisionTree Model Class

---
+++ src/src/models/DecisionTree.java
@@ -0,0 +1,18 @@
+package models;
+
+import core.Instance;
+import core.Model;
+
+import java.util.List;
+
+public class DecisionTree<F, L> implements Model<F, L> {
+    @Override
+    public void train(List<Instance<F, L>> instances) {
+
+    }
+
+    @Override
+    public void test(List<Instance<F, L>> instances) {
+
+    }
+}
